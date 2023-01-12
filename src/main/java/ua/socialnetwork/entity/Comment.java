dfbb9a2ee0b5b7d68754b198f43fe2f44051ef2e
cumbainer <shtaiier228666@gmail.com>
2023-01-12 16:05:22 +0200

    + uncompleted friend entitty

--- src/main/java/ua/socialnetwork/entity/Comment.java
+++ src/main/java/ua/socialnetwork/entity/Comment.java
@@ -30,7 +30,6 @@ public class Comment {
 
     @Column(name = "editionDate")
     private LocalDateTime editionDate;
-
     @ManyToOne
     @JoinColumn(name = "post_id",nullable = false)
     private Post post;
