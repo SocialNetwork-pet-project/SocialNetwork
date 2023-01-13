89d7a4410dd93e33d122c8c9587cd7e71048f673
cumbainer <shtaiier228666@gmail.com>
2023-01-13 20:29:53 +0200

    added custom login page

--- src/main/java/ua/socialnetwork/security/SecurityConfig.java
+++ src/main/java/ua/socialnetwork/security/SecurityConfig.java
@@ -1,44 +1,55 @@
-//package ua.socialnetwork.security;
-//
-//
-//import lombok.AllArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//
-//@Configuration
-//@EnableMethodSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests().requestMatchers("/posts").permitAll()
-//                .and();
-////                .formLogin()
-////                .loginPage("/login")
-////                .defaultSuccessUrl("/posts")
-////                .and()
-////                .logout();//ToDo logout implement later
-//
-//        return http.build();
-//    }
-//
+package ua.socialnetwork.security;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests().requestMatchers("/posts").permitAll()
+                .and()
+                .authorizeHttpRequests().anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/posts")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutSuccessUrl("/login");
+
+//                .and()
+//                .logout();//ToDo logout implement later
+
+        return http.build();
+    }
+
 //    @Bean
 //    public PasswordEncoder passwordEncoder() {
 //        return new BCryptPasswordEncoder(7);
 //    }
-//
-//
-//
-//
-//
-//
-//
-//}
+
+
+
+
+
+
+
+}
