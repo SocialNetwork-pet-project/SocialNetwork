db9e4b2a1ca8c1c4fa5e69936e88af0418b07b85
cumbainer <shtaiier228666@gmail.com>
2023-01-28 16:12:57 +0200

    added custom error pages + update on profile

---
+++ src/main/java/ua/socialnetwork/controller/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+//package ua.socialnetwork.controller;
+//
+//
+//import com.vaadin.flow.router.NotFoundException;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//
+//    @ExceptionHandler(NotFoundException.class)
+//    public String handle404Exception(Model model){
+//
+//        model.addAttribute("")
+//
+//
+//        return "404";
+//    }
+//}
